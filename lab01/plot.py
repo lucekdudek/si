cb1ed7f08ca80d2e7198d2b8bdd012b1108d3d78
lucekdudek <lucekdudek@github.com>
2016-06-28 22:37:32 +0200

    [lab01] matplotlib usage

---
+++ lab01/plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+
+class LabPlot:
+
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
